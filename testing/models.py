a4d096bc93b54f56a1e6db1e142360e4e77de54c
Stefan Richardson <sc16sr@leeds.ac.uk>
2018-02-13 13:18:01 +0000

    basic movie database set up, should allow UI's to use data

--- testing/models.py
+++ testing/models.py
@@ -1,8 +1,11 @@
 from testing import db
 import datetime
 
-class Moviles(db.Model):
+class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     movieTitle = db.Column(db.String(150))
     synopsis = db.Column(db.String(1000))
     rating = db.Column(db.Float)
+
+    def __repr__(self):
+        return '' % (self.id, self.movieTitle, self.synopsis, self.rating)
