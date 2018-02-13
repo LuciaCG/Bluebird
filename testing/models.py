ade1aa0eed4e8a0f7e8076d2e8abb8d6c052dff1
Freddie Richardson <sc15fr@leeds.ac.uk>
2018-02-13 13:04:36 +0000

    basic movie table completed

--- testing/models.py
+++ testing/models.py
@@ -3,4 +3,6 @@ import datetime
 
 class Moviles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    movieTitle = db.Column(db.String(150))
+    synopsis = db.Column(db.String(1000))
+    rating = db.Column(db.Float)
