e1ee960ae79562914912320d5f27bdb8dab377a2
Stefan Richardson <sc16sr@leeds.ac.uk>
2018-03-01 16:01:30 +0000

    Screening table added

--- app/models.py
+++ app/models.py
@@ -6,6 +6,19 @@ class Movies(db.Model):
     movieTitle = db.Column(db.String(150))
     synopsis = db.Column(db.String(1000))
     rating = db.Column(db.Float)
+    screen = db.relationship('Screenings', backref='movies', lazy='dynamic')
 
     def __repr__(self):
         return '' % (self.id, self.movieTitle, self.synopsis, self.rating)
+
+
+class Screenings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movies_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+    ##ForeignKey screenid
+    time = db.Column(db.DateTime)
+    # seatReserved = db.relationship('SeatReserved', backref='screenings', lazy='dynamic')
+    # userReciept = db.relationship('Reciept', backref='screenings', lazy='dynamic')
+
+    def __repr__(self):
+        return '' % (self.id, self.movies_id, self.time)
