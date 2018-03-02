676b39aad0595f2435b0c7ac2299f73d82d64b0f
Kavangh <sc16rk@leeds.ac.uk>
2018-03-02 13:52:23 +0000

    Added comments to the code

--- QT/mainwindow.cpp
+++ QT/mainwindow.cpp
@@ -11,31 +11,39 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    //Initialising the data base connection
     QSqlDatabase firstDB = QSqlDatabase::addDatabase("QSQLITE");
     firstDB.setHostName("bluebird");
+    //Database location
     firstDB.setDatabaseName("/home/csunix/sc16rk/Year2/Project/bluebird/app.db");
+    //Checks database connection
     firstDB.open();
     if(!firstDB.open())
         ui->label->setText("FAILED");
      else
         ui->label->setText("Connected");
 
+    //Creating an SQL table model
     QSqlTableModel *model = new QSqlTableModel(this,firstDB);
+    //Selecting the Table we want from DB
     model->setTable("Movies");
-    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     model->select();
-    model->setHeaderData(0, Qt::Horizontal, tr("ID"));
-    model->setHeaderData(1, Qt::Horizontal, tr("Movie Title"));
+    //Displaying the table in the Tableview
     ui->tableView->setModel(model);
     ui->tableView->show();
 
 
     QTimer *timer = new QTimer(this);
+    //Sets an delay between each updateS
     timer->setInterval(1000);
+    //using connect time to update the label
     connect(timer, &QTimer::timeout, [&]() {
+       //Getting current time from the system and turning into string to be displayed
        QString ct = QTime::currentTime().toString();
+       //setting the clock label to the current time
        ui->clock->setText(ct);
     } );
+    //updates the clock
     timer->start();
 
 }
