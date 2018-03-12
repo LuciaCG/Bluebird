5a6c9879216377da5a30d85823b6eb82e1257f0d
Kavangh <sc16rk@leeds.ac.uk>
2018-03-12 15:19:19 +0000

    Db issues fixed databse showing as planned

--- populatingDatabase.py
+++ populatingDatabase.py
@@ -4,12 +4,12 @@ import datetime
 
 ############## useful queries ###############
 
-from app import models, db
-import datetime
-for movies in models.Movies.query.all():
-  print(movies.movieTitle)
-  print(movies.synopsis)
-  print(movies.rating)
+# from app import models, db
+# import datetime
+# for movies in models.Movies.query.all():
+#   print(movies.movieTitle)
+#   print(movies.synopsis)
+#   print(movies.rating)
 #
 # from app import models, db
 # import datetime
@@ -57,45 +57,45 @@ db.session.commit()
 ############### adding screenings to Screenings tables ##############
 #setting times for screenings
 ## 02/03/2018 at 6pm
-screeningTime1 = datetime.datetime(2018, 2, 18, 12, tzinfo=datetime.timezone.utc)
-screeningTime2 = datetime.datetime(2018, 2, 18, 18, tzinfo=datetime.timezone.utc)
-screeningTime3 = datetime.datetime(2018, 2, 18, 20, tzinfo=datetime.timezone.utc)
-screeningTime4 = datetime.datetime(2018, 2, 19, 12, tzinfo=datetime.timezone.utc)
-screeningTime5 = datetime.datetime(2018, 2, 19, 18, tzinfo=datetime.timezone.utc)
-screeningTime6 = datetime.datetime(2018, 2, 19, 20, tzinfo=datetime.timezone.utc)
-screeningTime7 = datetime.datetime(2018, 3, 2, 9, tzinfo=datetime.timezone.utc)
-screeningTime8 = datetime.datetime(2018, 3, 2, 14, tzinfo=datetime.timezone.utc)
-screeningTime9 = datetime.datetime(2018, 3, 2, 21, tzinfo=datetime.timezone.utc)
-
-#assigning times to black panther
-blackPatherID = models.Movies.query.filter_by(movieTitle="Black Panther").first().id
-blackPantherScreening1 = models.Screenings(movies_id=blackPatherID, time = screeningTime1)
-blackPantherScreening2 = models.Screenings(movies_id=blackPatherID, time = screeningTime2)
-blackPantherScreening3 = models.Screenings(movies_id=blackPatherID, time = screeningTime3)
-blackPantherScreening4 = models.Screenings(movies_id=blackPatherID, time = screeningTime4)
-blackPantherScreening5 = models.Screenings(movies_id=blackPatherID, time = screeningTime5)
-blackPantherScreening6 = models.Screenings(movies_id=blackPatherID, time = screeningTime6)
-
-#assigning times to the greatest showman
-theGreatestShowmanID = models.Movies.query.filter_by(movieTitle="The Greatest Showman").first().id
-theGreatestShowmanScreening1 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime7)
-theGreatestShowmanScreening2 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime8)
-theGreatestShowmanScreening3 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime9)
+# screeningTime1 = datetime.datetime(2018, 2, 18, 12, tzinfo=datetime.timezone.utc)
+# screeningTime2 = datetime.datetime(2018, 2, 18, 18, tzinfo=datetime.timezone.utc)
+# screeningTime3 = datetime.datetime(2018, 2, 18, 20, tzinfo=datetime.timezone.utc)
+# screeningTime4 = datetime.datetime(2018, 2, 19, 12, tzinfo=datetime.timezone.utc)
+# screeningTime5 = datetime.datetime(2018, 2, 19, 18, tzinfo=datetime.timezone.utc)
+# screeningTime6 = datetime.datetime(2018, 2, 19, 20, tzinfo=datetime.timezone.utc)
+# screeningTime7 = datetime.datetime(2018, 3, 2, 9, tzinfo=datetime.timezone.utc)
+# screeningTime8 = datetime.datetime(2018, 3, 2, 14, tzinfo=datetime.timezone.utc)
+# screeningTime9 = datetime.datetime(2018, 3, 2, 21, tzinfo=datetime.timezone.utc)
+#
+# #assigning times to black panther
+# blackPatherID = models.Movies.query.filter_by(movieTitle="Black Panther").first().id
+# blackPantherScreening1 = models.Screenings(movies_id=blackPatherID, time = screeningTime1)
+# blackPantherScreening2 = models.Screenings(movies_id=blackPatherID, time = screeningTime2)
+# blackPantherScreening3 = models.Screenings(movies_id=blackPatherID, time = screeningTime3)
+# blackPantherScreening4 = models.Screenings(movies_id=blackPatherID, time = screeningTime4)
+# blackPantherScreening5 = models.Screenings(movies_id=blackPatherID, time = screeningTime5)
+# blackPantherScreening6 = models.Screenings(movies_id=blackPatherID, time = screeningTime6)
+#
+# #assigning times to the greatest showman
+# theGreatestShowmanID = models.Movies.query.filter_by(movieTitle="The Greatest Showman").first().id
+# theGreatestShowmanScreening1 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime7)
+# theGreatestShowmanScreening2 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime8)
+# theGreatestShowmanScreening3 = models.Screenings(movies_id=theGreatestShowmanID, time = screeningTime9)
 
 
 
 
 #adding and commit the screenings to the database
-db.session.add(blackPantherScreening1)
-db.session.add(blackPantherScreening2)
-db.session.add(blackPantherScreening3)
-db.session.add(blackPantherScreening4)
-db.session.add(blackPantherScreening5)
-db.session.add(blackPantherScreening6)
-
-db.session.add(theGreatestShowmanScreening1)
-db.session.add(theGreatestShowmanScreening2)
-db.session.add(theGreatestShowmanScreening3)
-db.session.commit()
+# db.session.add(blackPantherScreening1)
+# db.session.add(blackPantherScreening2)
+# db.session.add(blackPantherScreening3)
+# db.session.add(blackPantherScreening4)
+# db.session.add(blackPantherScreening5)
+# db.session.add(blackPantherScreening6)
+#
+# db.session.add(theGreatestShowmanScreening1)
+# db.session.add(theGreatestShowmanScreening2)
+# db.session.add(theGreatestShowmanScreening3)
+# db.session.commit()
 
 ############### adding screenings to Screenings tables ##############
