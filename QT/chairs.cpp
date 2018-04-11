9f906cad6937a679b201ef603341295549dfcabc
Kavangh <sc16rk@leeds.ac.uk>
2018-04-11 15:14:23 +0100

    Begining of seats selection database  display

--- QT/chairs.cpp
+++ QT/chairs.cpp
@@ -1,6 +1,7 @@
 #include "chairs.h"
 #include "ui_chairs.h"
 
+#include <QSqlTableModel>
 #include <QtWidgets>
 
 chairs::chairs(QWidget *parent, QString _time, int _id) :
@@ -12,7 +13,22 @@ chairs::chairs(QWidget *parent, QString _time, int _id) :
     ui->setupUi(this);
 
 
+    //Initialising the data base connection
+    QSqlDatabase firstDB = QSqlDatabase::addDatabase("QSQLITE");
+    firstDB.setHostName("bluebird");
+        //getting the relative path of the database
+    QDir bluebird = QDir::current();
+    bluebird.cdUp();
+    QString database = bluebird.path();
+    firstDB.setDatabaseName(database + "/app.db");
+        //connecting
+    firstDB.open();
+    if(!firstDB.open())
+        ui->label_3->setText("FAILED");
+     else
+        ui->label_3->setText("Connected");
 
+    // CLOCK
     QTimer *timer = new QTimer(this);
     //Sets an delay between each updateS
     timer->setInterval(1000);
@@ -25,6 +41,26 @@ chairs::chairs(QWidget *parent, QString _time, int _id) :
     } );
     //updates the clock
     timer->start();
+
+    // SEAT SELECTION
+    //Creating an SQL table model
+    QSqlTableModel *model = new QSqlTableModel(this,firstDB);
+    //Selecting the Table we want from DB
+    model->setTable("Seats");
+    model->select();
+
+    //Aesthetics
+    //model->setHeaderData(0, Qt::Horizontal, QObject::tr("Title"));
+    //model->setHeaderData(1, Qt::Horizontal, QObject::tr("Rating"));
+
+    //Displaying the table in the Tableview
+    ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableView->setModel(model);
+
+    ui->tableView->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView->show();
+
 }
 
 chairs::~chairs()
